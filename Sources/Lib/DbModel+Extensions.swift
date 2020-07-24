20afa2f16635697d214636c749a73fce24347a81
tony <anthony.chinwo@gmail.com>
2020-07-24 14:38:41 +0100

    - added demos playground to project

--- Sources/Lib/DbModel+Extensions.swift
+++ Sources/Lib/DbModel+Extensions.swift
@@ -257,53 +257,11 @@ enum Endpoint {
     
 }
 
-// MARK: - Playable
-public protocol Playable {
-    //associatedtype Track: Playable = Never
-    
-    var explicit: Bool? { get }
-    var title: String { get }
-    var thumbnailUrl: String { get }
-    var albumCoverUrl: String { get }
-    var artistName: String { get }
-    var uri: String { get }
-    var isPlayable: Bool { get }
-    var duration: Int? { get }
-    func play(deviceId: String?, positionMs: Int?, baseUrl: URL?, urlSession: URLSession?, on: DispatchQueue?) -> Promise<Json>
-}
 
 //extension Playable where Playable.Track == Track {
 //
 //}
 
-extension Playable {
-    
-    public var isPlayable: Bool {
-        return true
-    }
-    
-    @discardableResult
-    public func play(deviceId: String?, positionMs: Int? = nil, baseUrl: URL? = nil, urlSession: URLSession? = nil, on: DispatchQueue? = nil) -> Promise<Json> {
-            
-        var urlPath = URLComponents(string: "/api/spotify/play")!
-        urlPath.queryItems = [
-            URLQueryItem(name: "trackId", value: self.uri),
-        ]
-        
-        if let deviceId = deviceId {
-            urlPath.queryItems!.append(URLQueryItem(name: "deviceId", value: deviceId))
-        }
-        
-        var payload: Json = [:]
-        
-        if let positionMs = positionMs {
-            payload["position_ms"] = positionMs as AnyObject
-        }
-        
-        return HttpMethod.post.fetchJson(urlPath: urlPath, payload: payload, baseUrl: baseUrl, urlSession: urlSession, on: on)
-    }
-    
-}
 
 public var BASE_URL: (ws: URL, http: URL)!
 
