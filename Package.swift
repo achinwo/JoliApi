f4d945008c415317d77614682fbece0edb39e7e9
tony <anthony.chinwo@gmail.com>
2021-05-12 11:16:11 +0100

    - added tvOS build target

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [
         .macOS(.v11),
         .iOS(.v14),
-        .watchOS(.v7)
+        .watchOS(.v7),
+        .tvOS(.v14),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
